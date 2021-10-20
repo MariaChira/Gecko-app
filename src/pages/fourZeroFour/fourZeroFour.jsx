5e8bf9a6b54978ba13c172bf561e9e5c74f4f989
Maria Chira <geaguleamaria@yahoo.com>
2021-10-20 20:50:11 +0300

    add router, pages, components

---
+++ src/pages/fourZeroFour/fourZeroFour.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+const FourZeroFour = () => {
+    return (
+        <div>FourZeroFour</div>
+    )
+}
+
+export default FourZeroFour;
--- src/services/api.js
+++ src/services/api.js
@@ -2,35 +2,39 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://api.coingecko.com/api/v3',
-    headers: {'Accept': 'application/json' }
-
+    headers: {'Accept': 'application/json'}
 });
 
 const buildParams = (params) => {
-    if (params === undefined) return '----'
-    console.log(params)
+    if(params === undefined)return ''
     const arr = [];
     Object.keys(params).forEach(k => {
-       if(params[k])arr.push(`${k}=${params[k]}`)
-    })
+        if(params[k])arr.push(`${k}=${params[k]}`)
+    });
     return '?'+arr.join('&');
 }
 
-
- export const getCoinsMarket = async (params) => {
-     let isValid = params !== undefined;
+export const getCoinsMarket = async (params) => {
+    let isValid = params !== undefined;
     if(params){
-      const paramsArray = Object.keys(params);
-     isValid = paramsArray.includes('vs_currency');
-    
+        const paramsArray = Object.keys(params);
+        isValid = paramsArray.includes('vs_currency');
     }
-    if(isValid) {
+    if(isValid){
         const parsedParams = buildParams(params)
-        console.log(parsedParams)
-        return await instance.get(`/coins/markets?${parsedParams}`);
+        try{
+            const res = await instance.get(`/coins/markets${parsedParams}`);
+            return { data: res.data };
+        }
+        catch(error){
+            return { error }
+        }       
+    }  
+    else {
+        return {
+            error: 'Invalid call parameters'
+        }
     }
-
-  
 }
 
  
